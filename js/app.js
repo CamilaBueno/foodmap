25f6d32af605126f499cf64d6008402fe20e0d5c
Camila Bueno <cbf.camila@gmail.com>
2018-10-30 15:35:06 -0300

    Alterações na identação

--- js/app.js
+++ js/app.js
@@ -1,14 +1,10 @@
-// Add your JavaScript
-$(document).ready(function() {
-  $('.box-2').delay('3000').fadeIn('slow')
 
- 
-  
+$(document).ready(function () {
+  $('.box-2').delay('3000').fadeIn('slow')
 });
 //Show Restaurants
 function showRestaurants(restaurantsArray) {
   $('#img-restaurant').html('');
-
   $.each(restaurantsArray, function (index, restaurantes) {
     $('<img>')
       .attr('src', restaurantes.image)
@@ -21,80 +17,68 @@ function showRestaurants(restaurantsArray) {
 //Modal
 function openModal(event) {
   var restaurantName = $(event.target).attr('id');
-
- var restaurantData = 
+  var restaurantData =
     $(restaurantes).filter(function (index, restaurantes) {
       if (restaurantName === restaurantes.name) {
         return true;
       } else {
-        return false; 
+        return false;
       }
     })[0];
-    $('#modal-restaurant-name').text(restaurantData.name);
-    $('#modal-restaurant-type').text(restaurantData.type);
-    $('#modal-restaurant-description').text(restaurantData.description);
-    $('#modal-restaurant-image').attr('src', restaurantData.image);
+  $('#modal-restaurant-name').text(restaurantData.name);
+  $('#modal-restaurant-type').text(restaurantData.type);
+  $('#modal-restaurant-description').text(restaurantData.description);
+  $('#modal-restaurant-image').attr('src', restaurantData.image);
 
-    $('#modal-restaurant').modal('show');
-
-    $('.form-control').val('');
-  
-    showRestaurants(restaurantes);
-  }
-//searchRestaurants
-  function search() {
-    $('.form-control').keyup(function() {
-      var search = $(this).val().toLowerCase();
-      
-      var filteredRestaurants = 
-        $(restaurantes).filter(function (index, restaurant) { 
-         
-          if (search === '') { 
-            return true;
-          }
-  
-          if (restaurant.type.toLowerCase() === search) {
-            return true;
-          } else {
-            return false;
-          }
-        })
-  
-      $('#img-restaurant').html('');
-  
-      showRestaurants(filteredRestaurants);
-    });
-  }
-  $(document).ready(function() {
-    showRestaurants(restaurantes);
-   search();
+  $('#modal-restaurant').modal('show');
+  $('.form-control').val('');
+  showRestaurants(restaurantes);
+}
+//SearchRestaurants
+function search() {
+  $('.form-control').keyup(function () {
+    var search = $(this).val().toLowerCase();
+    var filteredRestaurants =
+      $(restaurantes).filter(function (index, restaurant) {
+        if (search === '') {
+          return true;
+        }
+        if (restaurant.type.toLowerCase() === search) {
+          return true;
+        } else {
+          return false;
+        }
+      })
+    $('#img-restaurant').html('');
+    showRestaurants(filteredRestaurants);
   });
+}
+$(document).ready(function () {
+  showRestaurants(restaurantes);
+  search();
+});
 //map  
 function initMap() {
- 
-    map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 14,
-      center: new google.maps.LatLng(-23.5578108, -46.6625469),
-      mapTypeId: 'roadmap'
+  map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 14,
+    center: new google.maps.LatLng(-23.5578108, -46.6625469),
+    mapTypeId: 'roadmap'
+  });
+  var features = restaurantes.map(function (r) {
+    return {
+      position: new google.maps.LatLng(r.latitude, r.longitude),
+      type: 'info'
+    }
+  });
+  // Markers.
+  features.forEach(function (feature) {
+    var marker = new google.maps.Marker({
+      position: feature.position,
+      map: map
     });
-
-    
-    var features = restaurantes.map(function(r){
-      return {
-          position: new google.maps.LatLng(r.latitude, r.longitude),
-          type: 'info'
-        }
-    });
-
-    // Markers.
-    features.forEach(function(feature) {
-      var marker = new google.maps.Marker({
-        position: feature.position,
-        map: map
-      });
-    });
-  }
+  });
+}
 
 
-  
- 
+
+
